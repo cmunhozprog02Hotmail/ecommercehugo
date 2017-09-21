e72b4c882e842e744f4a907359ca5f26e9034a71
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-09-21 16:22:58 -0300

    Aula 09 - Completa com alteraçoes de nvarchar para varchar

--- ECommerce/ECommerce/Models/EcommerceContext.cs
+++ ECommerce/ECommerce/Models/EcommerceContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ECommerce.Models
 {
@@ -9,7 +10,23 @@ namespace ECommerce.Models
         }
         public DbSet<Departaments> Departaments { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
 
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
