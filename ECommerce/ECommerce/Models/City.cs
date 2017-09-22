d749742acdf0a5e495dea5ccb720c0dfc38c3f93
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-09-22 13:01:23 -0300

    Aula 10 - Completa

---
+++ ECommerce/ECommerce/Models/City.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+
+        [Required(ErrorMessage = "O Campo Nome é requerido!")]
+        public string Name { get; set; }
+
+        public int DepartamentsId { get; set; }
+    }
+}
